c6965017ad5acb434c636a1ec7460fdd0edd9a3a
eastsea8366 <eastsea8366@gmail.com>
2026-08-14 19:03:00 +0900

    Enable CAD marquee mouse controls

--- src/ui.js
+++ src/ui.js
@@ -3,6 +3,7 @@ import {STLLoader} from "three/addons/loaders/STLLoader.js";
 import {CAD} from "./core.js";
 import "./selection.js";
 import "./sidebar.js";
+import "./input-controls.js";
 
 CAD.$("applySpace").onclick=()=>CAD.rebuildSpace(true);
 CAD.$("buildFrame").onclick=CAD.buildFrame;
